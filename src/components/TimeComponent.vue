c5684c6329bc546b8ce3a6388b985b5388aad7a7
shiimu <siim.laineste@outlook.com>
2021-12-22 14:12:01 +0200

    TimeComponent added + style.css

---
+++ src/components/TimeComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul class="list-group">
+      <li class="list-group-item" v-show="isDate">
+        <strong></strong> {{ date }}
+      </li>
+      <li class="list-group-item" v-show="isTime">
+        <strong></strong> {{ time }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TimeComponent",
+  data: () => ({
+    date: "",
+    time: "",
+    isDate: true,
+    isTime: true,
+  }),
+  methods: {
+    printDate: function () {
+      return new Date().toLocaleDateString();
+    },
+    printTime: function () {
+      return new Date().toLocaleTimeString();
+    },
+  },
+  mounted: function () {
+    this.date = this.printDate();
+    this.time = this.printTime();
+  },
+};
+</script>
